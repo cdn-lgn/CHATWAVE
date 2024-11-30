6e967a9d6fe9cc75d6325071620d838bddef27ba
cdn-lgn <logan2246t@gmail.com>
2024-11-30 06:48:19 +0530

    offline system commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
