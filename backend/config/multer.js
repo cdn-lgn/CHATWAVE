43bcd75669ba755b4e252e9bfdd5216e79b219ab
Logan <149867954+cdn-lgn@users.noreply.github.com>
2024-12-24 14:10:02 +0000

    merging conflict

--- backend/config/multer.js
+++
@@ -1,6 +0,0 @@
-import multer from "multer";
-
-const storage = multer.memoryStorage(); // Store the file in memory as a buffer
-const upload = multer({ storage }); // Use memory storage for uploads
-
-export default upload;
--- backend/routes/userStatusRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import authMiddleware from "../middleware/authMiddleware.js"
-import { userStatus } from '../controllers/userStatusController.js';
-
-const router = express.Router()
-
-router.get("/:id",authMiddleware,userStatus)
-
-export default router
