49972833fd44cfeef8470a25eaab9f31d42458a7
cdn-lgn <logan2246t@gmail.com>
2024-12-27 10:19:49 +0530

    vercel.json added

--- backend/index.js
+++ backend/index.js
@@ -39,8 +39,6 @@ app.use(
     cors({
         origin: process.env.FRONTEND_URL || "http://localhost:5173",
         credentials: true,
-        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], // Allow methods for CORS
-        allowedHeaders: ["Content-Type", "Authorization", "Origin", "Accept"],
     }),
 );
 
