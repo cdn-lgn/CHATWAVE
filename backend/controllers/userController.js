c42700f8c30a61102407a21efa8678ec66d5a27c
Logan <149867954+cdn-lgn@users.noreply.github.com>
2025-04-17 10:18:29 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -64,7 +64,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "None",
+      sameSite: "none",
     });
 
     // Convert user to plain object
