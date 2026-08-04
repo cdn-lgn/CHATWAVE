e863fba1ce5f65032a946fa5053d7f33e22c6756
LOGAN <iam.logan.developer@gmail.com>
2026-08-04 07:53:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -44,10 +44,10 @@ app.use(
     }),
 );
 
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log(`${req.method} ${req.url}`);
+//     next();
+// });
 
 // Routes
 app.use("/api/user", userAuthRoute);
