068fdb84b6d708176e4865ff398f73788f93454e
LOGAN <iam.logan.developer@gmail.com>
2026-08-04 08:48:21 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,19 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectDB = async () => {
+	if (isConnected) return;
+
 	try {
-		await mongoose.connect(process.env.MONGO_URI);
+		const db = await mongoose.connect(process.env.MONGO_URI);
+
+		isConnected = db.connections[0].readyState === 1;
+
 		console.log("MongoDB connected");
 	} catch (error) {
-		console.error(error.message);
-		process.exit(1);
+		console.error("MongoDB error:", error.message);
+		throw error;
 	}
 };
 
